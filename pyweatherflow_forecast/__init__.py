5a980540c20ad4c39f09adb213e4035f33db2f4b
Bjarne Riis <bjarne@briis.com>
2023-09-02 10:38:34 +0200

    Updated with base info

--- pyweatherflow_forecast/__init__.py
+++ pyweatherflow_forecast/__init__.py
@@ -1,3 +1,7 @@
 """Python Wrapper for WeatherFlow Forecast API."""
 from __future__ import annotations
 
+__title__ = "PYWEATHERFLOWFORECAST"
+__version__ = "0.0.1"
+__author__ = "briis"
+__license__ = "MIT"
