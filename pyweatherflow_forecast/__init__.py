275be80489e038e45d8b389ca52e34da8333d57b
Bjarne Riis <bjarne@briis.com>
2023-09-17 15:36:07 +0000

    Bump to 0.2.0

--- pyweatherflow_forecast/__init__.py
+++ pyweatherflow_forecast/__init__.py
@@ -4,6 +4,6 @@ from pyweatherflow_forecast.wffcst_lib import WeatherFlow, WeatherFlowAPIBase
 from pyweatherflow_forecast.data import WeatherFlowForecastData, WeatherFlowForecastDaily, WeatherFlowForecastHourly, WeatherFlowStationData
 
 __title__ = "PYWEATHERFLOWFORECAST"
-__version__ = "0.0.2"
+__version__ = "0.2.0"
 __author__ = "briis"
 __license__ = "MIT"
