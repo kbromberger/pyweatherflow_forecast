9bca34fbbf8161001993799036b230bd0926e8ef
Bjarne Riis <bjarne@briis.com>
2023-09-18 05:55:11 +0000

    Bump to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyweatherflow_forecast_pkg",
-    version="0.2.1",
+    version="0.2.2",
     author="briis",
     author_email="bjarne@briis.com",
     description="Gets the weather forecast data from WeatherFlow",
