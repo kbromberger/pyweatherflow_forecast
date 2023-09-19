522df3cc4f8328652a57e55e41c809a2cce2b5da
Bjarne Riis <bjarne@briis.com>
2023-09-19 11:14:29 +0000

    Bump to 0.3.2

--- pyweatherflow_forecast/__init__.py
+++ pyweatherflow_forecast/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from pyweatherflow_forecast.wffcst_lib import WeatherFlow, WeatherFlowAPIBase
 from pyweatherflow_forecast.data import WeatherFlowForecastData, WeatherFlowForecastDaily, WeatherFlowForecastHourly, WeatherFlowStationData
 
-__title__ = "PYWEATHERFLOWFORECAST"
-__version__ = "0.3.1"
+__title__ = "PYWEATHERFLOW-FORECAST"
+__version__ = "0.3.2"
 __author__ = "briis"
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyweatherflow-forecast",
-    version="0.3.1",
+    version="0.3.2",
     author="briis",
     author_email="bjarne@briis.com",
     description="Gets the weather forecast data from WeatherFlow",
