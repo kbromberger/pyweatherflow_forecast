177a2934bf0b5f76449afff2c5dc37462f7b40fb
Bjarne Riis <bjarne@briis.com>
2023-09-28 07:29:24 +0200

    Added error handling exports

--- pyweatherflow_forecast/__init__.py
+++ pyweatherflow_forecast/__init__.py
@@ -1,7 +1,17 @@
 """Python Wrapper for WeatherFlow Forecast API."""
 from __future__ import annotations
-from pyweatherflow_forecast.wffcst_lib import WeatherFlow, WeatherFlowAPIBase
-from pyweatherflow_forecast.data import WeatherFlowForecastData, WeatherFlowForecastDaily, WeatherFlowForecastHourly, WeatherFlowStationData
+from pyweatherflow_forecast.wffcst_lib import (
+    WeatherFlow,
+    WeatherFlowForecastInternalServerError,
+    WeatherFlowForecastBadRequest,
+    WeatherFlowForecastUnauthorized,
+)
+from pyweatherflow_forecast.data import (
+    WeatherFlowForecastData,
+    WeatherFlowForecastDaily,
+    WeatherFlowForecastHourly,
+    WeatherFlowStationData,
+)
 
 __title__ = "pyweatherflow_forecast"
 __version__ = "0.3.3"
